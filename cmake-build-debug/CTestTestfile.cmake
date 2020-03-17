d1934d338c5950f98b085ba961794479d411a647
Jigao Luo_X1 <luojigao@outlook.com>
2020-03-17 17:04:34 +0100

    init project

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jigao/Workspace/CMU15-445-2017
+# Build directory: /home/jigao/Workspace/CMU15-445-2017/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
